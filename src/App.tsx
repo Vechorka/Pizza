e25baa187afa45b505e0d638eb21d0e21a596a75
Vechorka <vechorkagroup@gmail.com>
2023-04-23 21:12:56 +0300

    init: fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './scss/app.scss';
 import {Header} from "./components/Header/Header";
 import {Categories} from "./components/Categories/Categories";
 import {Sort} from "./components/Sort/Sort";
 import {Pizza} from "./components/Pizza/Pizza";
-import pizzas from './pizzas.json'
-
-
+import {isUint32Array} from "util/types";
 
 function App() {
+    let [items, setItems] = useState<any[]>([])
+
+    useEffect(()=>{
+        fetch('https://64456982b80f57f581b98c4e.mockapi.io/items')
+            .then(res => res.json())
+            .then((arr)=>{
+                setItems(arr)
+            })
+
+    },[])
+
+
     return (
         <div>
             <div className="wrapper">
@@ -22,7 +32,7 @@ function App() {
                         <h2 className='content__title'>All pizzas</h2>
                         <div className='content__items'>
                             {
-                                pizzas.map((obj)=> (<Pizza key={obj.id} title={obj.title} price={obj.price} image={obj.imageUrl}
+                                items.map((obj)=> (<Pizza key={obj.id} title={obj.title} price={obj.price} image={obj.imageUrl}
                                                            sizes={obj.sizes} types={obj.types}/>))
                             }
                         </div>
