cacf9cc5c50215eb5d903ff6c92fd35261f73432
Vechorka <vechorkagroup@gmail.com>
2023-04-27 21:52:06 +0300

    feat: css fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,11 +15,11 @@ export const Home = () => {
                 setItems(arr)
                 setIsLoading(false)
             })
-
+        window.scrollTo(0, 0)
     },[])
 
     return (
-        <>
+        <div className='container'>
             <div className={'content__top'}>
                 <Categories/>
                 <Sort/>
@@ -35,7 +35,7 @@ export const Home = () => {
                     ))
                 }
             </div>
-        </>
+        </div>
 
     )
 }
